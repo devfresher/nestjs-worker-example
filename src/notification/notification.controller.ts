52c8a01ed6cc34ab51be54bdedbfe83ca59b2131
devfresher <fresher.dev01@gmail.com>
2025-04-29 20:09:48 +0100

    Add notify endpoint to NotificationController
    - Introduced a new POST endpoint for notifications.
    - Accepts a payload of type NotificationPayload and an array of NotificationChannel.
    - Integrates with NotificationService to handle notification logic.

--- src/notification/notification.controller.ts
+++ src/notification/notification.controller.ts
@@ -1,7 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { NotificationService } from './notification.service';
+import { NotificationPayload } from './types/notification-payload';
+import { NotificationChannel } from './enums/channel.enum';
 
 @Controller('notification')
 export class NotificationController {
   constructor(private readonly notificationService: NotificationService) {}
+
+  @Post()
+  async notify(
+    @Body() payload: NotificationPayload,
+    @Body() channels: NotificationChannel[],
+  ) {
+    await this.notificationService.notify(payload, channels);
+  }
 }
